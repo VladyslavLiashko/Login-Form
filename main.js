8c5c6277575deef7dbfa38eebf71fdded866c074
Vlad <vladliashko@gmail.com>
2022-12-07 18:18:53 +0200

    add Sign Up page

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+let login = document.querySelector(".wrapper_inner");
+let signupBtn = document.querySelector("#signUp");
+let signUpSection = document.querySelector(".sign-up-wrapper");
+let loginBack = document.querySelector("#loginBack");
 let form = document.forms[0];
-form.mail.addEventListener("click", function(){
-    alert("kokok")
-})
+signupBtn.addEventListener("click", function () {
+    login.style.display = "none"
+    signUpSection.style.display = "block"
+});
+loginBack.addEventListener("click", returnFunc);
+form.signUpBtn.addEventListener("click", returnFunc);
+function returnFunc() {
+    login.style.display = "block"
+    signUpSection.style.display = "none"
+    if (form.pass.value != form.repeatPass.value) {
+        alert("Passwords are not same")
+    }
+}
+
+// document.querySelector("#checkbox").addEventListener("click", remember)
+
+// let rememberValue = localStorage.getItem("value");
+
+// if(rememberValue){
+//     form.mail.value = "rememberValue"
+// }
+
+// function remember(){
+//     let selectedMail = form.mail.value
+//     // let selectedPass = form.pswrd.value
+
+//     localStorage.setItem("value", selectedMail);
+//     // localStorage.setItem("value", selectedPass);
+// }
